71ac4a0dab95beecfd38b588816efd73447d1d19
harald.barsnes <harald.barsnes@d4655fc2-3d53-48af-725a-7f306d00f420>
2011-02-10 17:45:16 +0000

    Added an additional data type to the JSparklinesBarChartTableCellRenderer, XYDataPoint, making it possible to use significance color coding in addition to the up/down regulated colors. Also added default bar chart colors and created additional constructors.

---
+++ trunk/src/main/java/no/uib/jsparklines/data/XYDataPoint.java
@@ -0,0 +1,82 @@
+package no.uib.jsparklines.data;
+
+/**
+ * Object that stores data about one data point in an XY plot.
+ *
+ * @author Harald Barsnes
+ */
+public class XYDataPoint implements Comparable<XYDataPoint> {
+
+    private double x;
+    private double y;
+
+    /**
+     * Create a new XYDataPoint.
+     *
+     * @param x
+     * @param y
+     */
+    public XYDataPoint(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * @return the x
+     */
+    public double getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public double getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    /**
+     * Returns the xy-values as a string. Note that the values are rounded
+     * to two decimals.
+     *
+     * @return the xy-values as a string
+     */
+    public String toString() {
+        return "(" + roundDouble(x, 2) + "," + roundDouble(y, 2) + ")";
+    }
+
+    /**
+     * Rounds of a double value to the wanted number of decimalplaces
+     *
+     * @param d the double to round of
+     * @param places number of decimal places wanted
+     * @return double - the new double
+     */
+    private static double roundDouble(double d, int places) {
+        return Math.round(d * Math.pow(10, (double) places)) / Math.pow(10, (double) places);
+    }
+
+    /**
+     * Compares based in the x-value.
+     *
+     * @param o
+     * @return
+     */
+    public int compareTo(XYDataPoint o) {
+        return Double.compare(this.x, o.x);
+    }
+}
