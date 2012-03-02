0e3a6492df5c2181ac64d9f649b868f961a1d45e
harald.barsnes <harald.barsnes@d4655fc2-3d53-48af-725a-7f306d00f420>
2012-03-02 17:22:52 +0000

    Added a pie chart version to the JSparklinesMultiLabelTableCellRenderer. Moved project to version 0.5.35.

--- src/main/java/no/uib/jsparklines/renderers/JSparklinesMultiLabelTableCellRenderer.java
+++ src/main/java/no/uib/jsparklines/renderers/JSparklinesMultiLabelTableCellRenderer.java
@@ -11,16 +11,21 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellRenderer;
 import no.uib.jsparklines.data.JSparklinesMultiLabelDataset;
+import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.chart.plot.Plot;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.LookupPaintScale;
 import org.jfree.chart.renderer.xy.XYBlockRenderer;
+import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.xy.DefaultXYZDataset;
 
 /**
- * A renderer for 1-4 color labels displayed as equal size bar charts inside a table cell. 
+ * A renderer for 1-4 color labels displayed either a pie chart or as equal 
+ * parts of a square, both inside a table cell.
  *
  * @author Harald Barsnes
  */
@@ -35,17 +40,32 @@ public class JSparklinesMultiLabelTableCellRenderer extends JPanel implements Ta
      */
     private JFreeChart chart;
     /**
-     * The background color used for the plots. For plots using light
-     * colors, it's recommended to use a dark background color, and for
-     * plots using darker colors it is recommended to use a light background.
+     * The background color used for the plots. For plots using light colors,
+     * it's recommended to use a dark background color, and for plots using
+     * darker colors it is recommended to use a light background.
      */
     private Color plotBackgroundColor = null;
+    /**
+     * If true a pie chart is used to display the label colors, false uses a
+     * square. False is the default.
+     */
+    private boolean circle = false;
 
     /**
      * Creates a new JSparklinesColorTableCellRenderer.
      */
     public JSparklinesMultiLabelTableCellRenderer() {
         setUpRendererAndChart();
+    }
+
+    /**
+     * If true a pie chart is used to display the label colors, false uses a
+     * square. False is the default.
+     *
+     * @param circle
+     */
+    public void setCircle(boolean circle) {
+        this.circle = circle;
     }
 
     /**
@@ -107,28 +127,50 @@ public class JSparklinesMultiLabelTableCellRenderer extends JPanel implements Ta
             } else if (dataset.getLabels().size() == 2) {
                 chart = createChart((Color) dataset.getLabels().get(0).getColor(),
                         (Color) dataset.getLabels().get(1).getColor());
-                tooltips = "<html><table border=\"0\">"
-                        + "<tr><td>" + dataset.getLabels().get(0).getLabel() + "</td>"
-                        + "<td>" + dataset.getLabels().get(1).getLabel() + "</td></tr></table></html>"; 
+                if (circle) {
+                    tooltips = "<html><table border=\"0\">"
+                            + "<tr><td>" + dataset.getLabels().get(1).getLabel() + "</td>"
+                            + "<td>" + dataset.getLabels().get(0).getLabel() + "</td></tr></table></html>";
+                } else {
+                    tooltips = "<html><table border=\"0\">"
+                            + "<tr><td>" + dataset.getLabels().get(0).getLabel() + "</td>"
+                            + "<td>" + dataset.getLabels().get(1).getLabel() + "</td></tr></table></html>";
+                }
             } else if (dataset.getLabels().size() == 3) {
                 chart = createChart((Color) dataset.getLabels().get(0).getColor(),
                         (Color) dataset.getLabels().get(1).getColor(),
                         (Color) dataset.getLabels().get(2).getColor());
-                tooltips = "<html><table border=\"0\">"
-                        + "<tr><td>" + dataset.getLabels().get(0).getLabel() + "</td>"
-                        + "<td>" + dataset.getLabels().get(1).getLabel() + "</td></tr>"
-                        + "<tr><td></td>"
-                        + "<td>" + dataset.getLabels().get(2).getLabel() + "</td></tr></table></html>"; 
+                if (circle) {
+                    tooltips = "<html><table border=\"0\">"
+                            + "<tr><td>" + dataset.getLabels().get(1).getLabel() + "</td>"
+                            + "<td>" + dataset.getLabels().get(0).getLabel() + "</td></tr>"
+                            + "<tr><td>" + dataset.getLabels().get(2).getLabel() + "</td>"
+                            + "<td></td></tr></table></html>";
+                } else {
+                    tooltips = "<html><table border=\"0\">"
+                            + "<tr><td>" + dataset.getLabels().get(0).getLabel() + "</td>"
+                            + "<td>" + dataset.getLabels().get(1).getLabel() + "</td></tr>"
+                            + "<tr><td></td>"
+                            + "<td>" + dataset.getLabels().get(2).getLabel() + "</td></tr></table></html>";
+                }
             } else if (dataset.getLabels().size() == 4) {
                 chart = createChart((Color) dataset.getLabels().get(0).getColor(),
                         (Color) dataset.getLabels().get(1).getColor(),
                         (Color) dataset.getLabels().get(2).getColor(),
                         (Color) dataset.getLabels().get(3).getColor(), true);
-                tooltips = "<html><table border=\"0\">"
-                        + "<tr><td>" + dataset.getLabels().get(0).getLabel() + "</td>"
-                        + "<td>" + dataset.getLabels().get(1).getLabel() + "</td></tr>"
-                        + "<tr><td>" + dataset.getLabels().get(3).getLabel() + "</td>"
-                        + "<td>" + dataset.getLabels().get(2).getLabel() + "</td></tr></table></html>";    
+                if (circle) {
+                    tooltips = "<html><table border=\"0\">"
+                            + "<tr><td>" + dataset.getLabels().get(3).getLabel() + "</td>"
+                            + "<td>" + dataset.getLabels().get(0).getLabel() + "</td></tr>"
+                            + "<tr><td>" + dataset.getLabels().get(2).getLabel() + "</td>"
+                            + "<td>" + dataset.getLabels().get(1).getLabel() + "</td></tr></table></html>";
+                } else {
+                    tooltips = "<html><table border=\"0\">"
+                            + "<tr><td>" + dataset.getLabels().get(0).getLabel() + "</td>"
+                            + "<td>" + dataset.getLabels().get(1).getLabel() + "</td></tr>"
+                            + "<tr><td>" + dataset.getLabels().get(3).getLabel() + "</td>"
+                            + "<td>" + dataset.getLabels().get(2).getLabel() + "</td></tr></table></html>";
+                }
             } else {
                 throw new IllegalArgumentException("JSparklinesMultiLabelTableCellRenderer only supports JSparklinesMultiLabelDataset objects of size 1-4!");
             }
@@ -142,7 +184,7 @@ public class JSparklinesMultiLabelTableCellRenderer extends JPanel implements Ta
 
         // set the tooltip text
         this.setToolTipText(tooltips);
-        
+
 
         // respect focus and hightlighting
         setBorder(c.getBorder());
@@ -167,7 +209,7 @@ public class JSparklinesMultiLabelTableCellRenderer extends JPanel implements Ta
             chart.setBackgroundPaint(new Color(bg.getRed(), bg.getGreen(), bg.getBlue()));
             this.setBackground(new Color(bg.getRed(), bg.getGreen(), bg.getBlue()));
         }
-        
+
         // @TODO: perhaps the colors below should not be hardcoded...
 
         if (isSelected) {
@@ -184,9 +226,9 @@ public class JSparklinesMultiLabelTableCellRenderer extends JPanel implements Ta
 
     /**
      * Create the chart with one label.
-     * 
+     *
      * @param labelA
-     * @return 
+     * @return
      */
     private JFreeChart createChart(Color labelA) {
         return createChart(labelA, labelA, labelA, labelA, false);
@@ -194,10 +236,10 @@ public class JSparklinesMultiLabelTableCellRenderer extends JPanel implements Ta
 
     /**
      * Create the chart with two labels.
-     * 
+     *
      * @param labelA
      * @param labelB
-     * @return 
+     * @return
      */
     private JFreeChart createChart(Color labelA, Color labelB) {
         return createChart(labelA, labelA, labelB, labelB, false);
@@ -205,11 +247,11 @@ public class JSparklinesMultiLabelTableCellRenderer extends JPanel implements Ta
 
     /**
      * Create the chart with three labels.
-     * 
+     *
      * @param labelA
      * @param labelB
      * @param labelC
-     * @return 
+     * @return
      */
     private JFreeChart createChart(Color labelA, Color labelB, Color labelC) {
         return createChart(labelA, labelA, labelC, labelB, false);
@@ -217,70 +259,105 @@ public class JSparklinesMultiLabelTableCellRenderer extends JPanel implements Ta
 
     /**
      * Create the chart with four labels.
-     * 
+     *
      * @param labelA
      * @param labelB
      * @param labelC
      * @param labelD
-     * @param all   if true, the provided label order is used, otherwise the order 
-     *              is changed to make sure that the labels are correctly ordered
-     * @return 
+     * @param all if true, the provided label order is used, otherwise the order
+     * is changed to make sure that the labels are correctly ordered
+     * @return
      */
     private JFreeChart createChart(Color labelA, Color labelB, Color labelC, Color labelD, boolean all) {
 
-        LookupPaintScale paintScale = new LookupPaintScale(0, 4, Color.lightGray);
+        Plot plot;
 
-        if (all) {
-            paintScale.add(1.0, labelD);
-            paintScale.add(2.0, labelA);
-            paintScale.add(3.0, labelC);
-            paintScale.add(4.0, labelB);
+        if (circle) {
+
+            DefaultPieDataset pieDataset = new DefaultPieDataset();
+
+            pieDataset.setValue("A", 1);
+            pieDataset.setValue("B", 1);
+            pieDataset.setValue("C", 1);
+            pieDataset.setValue("D", 1);
+
+
+            // create the chart
+            chart = ChartFactory.createPieChart(null, pieDataset, false, false, false);
+
+            plot = ((PiePlot) chart.getPlot());
+            ((PiePlot) plot).setCircular(true);
+            ((PiePlot) plot).setLabelGenerator(null);
+            ((PiePlot) plot).setShadowXOffset(0);
+            ((PiePlot) plot).setShadowYOffset(0);
+            ((PiePlot) plot).setSectionOutlinesVisible(false);
+
+            // set the series colors
+            ((PiePlot) plot).setSectionPaint("A", labelA);
+            ((PiePlot) plot).setSectionPaint("B", labelB);
+            ((PiePlot) plot).setSectionPaint("C", labelC);
+            ((PiePlot) plot).setSectionPaint("D", labelD);
+
+            plot.setOutlineStroke(new BasicStroke(20));
+
+            chart.getPlot().setOutlineVisible(false);
+
         } else {
-            paintScale.add(1.0, labelA);
-            paintScale.add(2.0, labelB);
-            paintScale.add(3.0, labelC);
-            paintScale.add(4.0, labelD);
+
+            LookupPaintScale paintScale = new LookupPaintScale(0, 4, Color.lightGray);
+
+            if (all) {
+                paintScale.add(1.0, labelD);
+                paintScale.add(2.0, labelA);
+                paintScale.add(3.0, labelC);
+                paintScale.add(4.0, labelB);
+            } else {
+                paintScale.add(1.0, labelA);
+                paintScale.add(2.0, labelB);
+                paintScale.add(3.0, labelC);
+                paintScale.add(4.0, labelD);
+            }
+
+            NumberAxis xAxis = new NumberAxis(null);
+            NumberAxis yAxis = new NumberAxis(null);
+            XYBlockRenderer renderer = new XYBlockRenderer();
+
+            renderer.setPaintScale(paintScale);
+
+            double[][] tempXYZData = new double[3][4];
+
+            tempXYZData[0][0] = 0;
+            tempXYZData[1][0] = 0;
+            tempXYZData[2][0] = 1;
+
+            tempXYZData[0][1] = 0;
+            tempXYZData[1][1] = 1;
+            tempXYZData[2][1] = 2;
+
+            tempXYZData[0][2] = 1;
+            tempXYZData[1][2] = 0;
+            tempXYZData[2][2] = 3;
+
+            tempXYZData[0][3] = 1;
+            tempXYZData[1][3] = 1;
+            tempXYZData[2][3] = 4;
+
+            DefaultXYZDataset dataset2 = new DefaultXYZDataset();
+            dataset2.addSeries("Series 1", tempXYZData);
+
+            plot = new XYPlot(dataset2, xAxis, yAxis, renderer);
+            plot.setBackgroundPaint(Color.WHITE);
+            ((XYPlot) plot).setDomainGridlinePaint(Color.white);
+            ((XYPlot) plot).setRangeGridlinePaint(Color.white);
+
+            // hide unwanted chart details
+            ((XYPlot) plot).getRangeAxis().setVisible(false);
+            ((XYPlot) plot).getDomainAxis().setVisible(false);
+            ((XYPlot) plot).setRangeGridlinesVisible(false);
+            ((XYPlot) plot).setDomainGridlinesVisible(false);
+
+            plot.setOutlineStroke(new BasicStroke(20));
         }
-
-        NumberAxis xAxis = new NumberAxis(null);
-        NumberAxis yAxis = new NumberAxis(null);
-        XYBlockRenderer renderer = new XYBlockRenderer();
-
-        renderer.setPaintScale(paintScale);
-
-        double[][] tempXYZData = new double[3][4];
-
-        tempXYZData[0][0] = 0;
-        tempXYZData[1][0] = 0;
-        tempXYZData[2][0] = 1;
-
-        tempXYZData[0][1] = 0;
-        tempXYZData[1][1] = 1;
-        tempXYZData[2][1] = 2;
-
-        tempXYZData[0][2] = 1;
-        tempXYZData[1][2] = 0;
-        tempXYZData[2][2] = 3;
-
-        tempXYZData[0][3] = 1;
-        tempXYZData[1][3] = 1;
-        tempXYZData[2][3] = 4;
-
-        DefaultXYZDataset dataset2 = new DefaultXYZDataset();
-        dataset2.addSeries("Series 1", tempXYZData);
-
-        XYPlot plot = new XYPlot(dataset2, xAxis, yAxis, renderer);
-        plot.setBackgroundPaint(Color.WHITE);
-        plot.setDomainGridlinePaint(Color.white);
-        plot.setRangeGridlinePaint(Color.white);
-
-        // hide unwanted chart details
-        plot.getRangeAxis().setVisible(false);
-        plot.getDomainAxis().setVisible(false);
-        plot.setRangeGridlinesVisible(false);
-        plot.setDomainGridlinesVisible(false);
-
-        plot.setOutlineStroke(new BasicStroke(20));
 
         JFreeChart tempChart = new JFreeChart(null, plot);
         tempChart.removeLegend();
