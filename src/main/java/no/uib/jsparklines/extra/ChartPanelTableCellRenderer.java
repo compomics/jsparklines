10f04e17b08cd463644ce96fafd91393502ba07c
harald.barsnes <harald.barsnes@d4655fc2-3d53-48af-725a-7f306d00f420>
2012-06-13 08:06:04 +0000

    Added a ChartPanel table cell renderer making it possible to put any chart panel into a cell. Moved project to version 0.5.37.

---
+++ src/main/java/no/uib/jsparklines/extra/ChartPanelTableCellRenderer.java
@@ -0,0 +1,69 @@
+package no.uib.jsparklines.extra;
+
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
+import org.jfree.chart.ChartPanel;
+
+/**
+ * A table cell renderer for a ChartPanel. 
+ *
+ * @author Harald Barsnes
+ */
+public class ChartPanelTableCellRenderer extends JLabel implements TableCellRenderer {
+
+    /**
+     * A reference to a standard table cell renderer.
+     */
+    private TableCellRenderer delegate = new DefaultTableCellRenderer();
+
+    /**
+     * Create a new ChartPanelTableCellRenderer.
+     */
+    public ChartPanelTableCellRenderer() {
+    }
+
+    /**
+     * Sets up the cell renderer for the given cell.
+     *
+     * @param table
+     * @param value
+     * @param isSelected
+     * @param hasFocus
+     * @param row
+     * @param column
+     * @return the rendered cell
+     */
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+
+        JComponent c = (JComponent) delegate.getTableCellRendererComponent(table, value,
+                isSelected, hasFocus, row, column);
+
+        // respect focus and hightlighting
+        setBorder(c.getBorder());
+        setOpaque(c.isOpaque());
+        setBackground(c.getBackground());
+
+        ChartPanel chartPanel = (ChartPanel) value;
+
+        // respect cell highlighting
+        Color bg = c.getBackground();
+        chartPanel.getChart().getPlot().setBackgroundPaint(new Color(bg.getRed(), bg.getGreen(), bg.getBlue()));
+        chartPanel.setBackground(new Color(bg.getRed(), bg.getGreen(), bg.getBlue()));
+        chartPanel.getChart().setBackgroundPaint(new Color(bg.getRed(), bg.getGreen(), bg.getBlue()));
+
+
+        // add border when cell is selected
+//        if (hasFocus) {
+//            chartPanel.setBorder(c.getBorder());
+//        } else {
+//            chartPanel.setBorder(null);
+//        }
+
+        return chartPanel;
+    }
+}
