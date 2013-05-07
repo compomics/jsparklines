475a9dfe8993d93ec5b7262c3aa3aa6e06633669
harald.barsnes <harald.barsnes@d4655fc2-3d53-48af-725a-7f306d00f420>
2013-05-07 12:05:58 +0000

    Added a first draft of a ChromosomeTableCellRenderer. Moved project to version 0.6.1.

---
+++ src/main/java/no/uib/jsparklines/extra/ChromosomeTableCellRenderer.java
@@ -0,0 +1,70 @@
+package no.uib.jsparklines.extra;
+
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
+import no.uib.jsparklines.data.Chromosome;
+
+/**
+ * A table cell renderer for chromosomes, i.e., 1-n and X, Y, Z and W.
+ *
+ * @author Harald Barsnes
+ */
+public class ChromosomeTableCellRenderer extends JLabel implements TableCellRenderer {
+
+    /**
+     * Create a new ChartPanelTableCellRenderer.
+     */
+    public ChromosomeTableCellRenderer() {
+    }
+
+    /**
+     * Sets up the cell renderer for the given cell.
+     *
+     * @param table
+     * @param value
+     * @param isSelected
+     * @param hasFocus
+     * @param row
+     * @param column
+     * @return the rendered cell
+     */
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+
+        JComponent c = (JComponent) new DefaultTableCellRenderer().getTableCellRendererComponent(table, value,
+                isSelected, hasFocus, row, column);
+
+        // respect focus and hightlighting
+        setBorder(c.getBorder());
+        setOpaque(c.isOpaque());
+        setBackground(c.getBackground());
+
+        JLabel valueLabel = new JLabel();
+        valueLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        valueLabel.setFont(valueLabel.getFont().deriveFont(valueLabel.getFont().getSize() - 2f));
+
+        // respect focus and hightlighting
+        Color bg = c.getBackground();
+
+        valueLabel.setBorder(c.getBorder());
+        valueLabel.setOpaque(c.isOpaque());
+        valueLabel.setBackground(new Color(bg.getRed(), bg.getGreen(), bg.getBlue()));
+
+        // update the link color depending on if the row is selected or not
+        if (isSelected) {
+            valueLabel.setForeground(Color.WHITE); // @TODO: should not be hardcoded!!!
+        } else {
+            valueLabel.setForeground(Color.BLACK);
+        }
+
+        if (value != null && value instanceof Chromosome) {
+            valueLabel.setText("" + ((Chromosome) value).toString());
+        }
+
+        return valueLabel;
+    }
+}
