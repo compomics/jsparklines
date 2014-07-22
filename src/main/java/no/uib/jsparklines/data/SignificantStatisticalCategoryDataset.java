2cac65622d3123cc1526d10a8cd8836b834409bf
harald.barsnes <harald.barsnes@d4655fc2-3d53-48af-725a-7f306d00f420>
2014-07-22 19:49:41 +0000

    JSparklinesErrorBarChartTableCellRenderer: Added support for indicating significance. JSparklinesErrorBarChartTableCellRenderer: Added support for displaying the mean value and the chart. Moved project to version 0.8.0.

---
+++ src/main/java/no/uib/jsparklines/data/SignificantStatisticalCategoryDataset.java
@@ -0,0 +1,36 @@
+package no.uib.jsparklines.data;
+
+import org.jfree.data.statistics.DefaultStatisticalCategoryDataset;
+
+/**
+ * An extention of DefaultStatisticalCategoryDataset that adds a boolean
+ * indicating if the dataset is significant or not.
+ *
+ * @author Harald Barsnes
+ */
+public class SignificantStatisticalCategoryDataset extends DefaultStatisticalCategoryDataset {
+
+    /**
+     * A boolean indicating if the dataset is significant or not.
+     */
+    private Boolean significant;
+
+    /**
+     * Returns true if the dataset is significant. False if it is not. If not
+     * set returns null.
+     *
+     * @return true if the dataset is significant, null if not set
+     */
+    public Boolean isSignificant() {
+        return significant;
+    }
+
+    /**
+     * Set if the dataset is significant or not.
+     *
+     * @param significant if the dataset is significant or not
+     */
+    public void setSignificant(boolean significant) {
+        this.significant = significant;
+    }
+}
