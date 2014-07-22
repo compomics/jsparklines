ea0d2edf7bd4ac157aa1a9633733814fd5dea9c3
harald.barsnes <harald.barsnes@d4655fc2-3d53-48af-725a-7f306d00f420>
2014-07-22 19:49:41 +0000

    JSparklinesErrorBarChartTableCellRenderer: Added support for indicating significance. JSparklinesErrorBarChartTableCellRenderer: Added support for displaying the mean value and the chart. Moved project to version 0.8.0.

--- trunk/src/main/java/no/uib/jsparklines/renderers/JSparklinesErrorBarChartTableCellRenderer.java
+++ trunk/src/main/java/no/uib/jsparklines/renderers/JSparklinesErrorBarChartTableCellRenderer.java
@@ -5,13 +5,19 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 import javax.swing.BoxLayout;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellRenderer;
+import no.uib.jsparklines.data.SignificantStatisticalCategoryDataset;
 import no.uib.jsparklines.renderers.util.GradientColorCoding;
 import no.uib.jsparklines.renderers.util.StatisticalBarChartColorRenderer;
 import org.jfree.chart.ChartFactory;
@@ -24,7 +30,7 @@ import org.jfree.data.statistics.DefaultStatisticalCategoryDataset;
 /**
  * A renderer for displaying a JSparklines bar charts with error bars inside a
  * table cell. Assumes that the cell values are of type
- * DefaultStatisticalCategoryDataset.
+ * DefaultStatisticalCategoryDataset or SignificantStatisticalCategoryDataset.
  *
  * @author Harald Barsnes
  */
@@ -54,6 +60,10 @@ public class JSparklinesErrorBarChartTableCellRenderer extends JPanel implements
      * The label used to display the number and the bar chart at the same time.
      */
     private JLabel valueLabel;
+    /**
+     * The label used to display the significance.
+     */
+    private JLabel signifianceLabel;
     /**
      * The maximum value. Used to set the maximum range for the chart.
      */
@@ -85,6 +95,23 @@ public class JSparklinesErrorBarChartTableCellRenderer extends JPanel implements
      * same time.
      */
     private int widthOfValueLabel = 40;
+    /**
+     * If true the value of the chart is shown next to the bar chart.
+     */
+    private boolean showNumberAndChart = false;
+    /**
+     * The decimal format for use when showing the numbers.
+     */
+    private DecimalFormat numberFormat = new DecimalFormat("0.00");
+    /**
+     * The horizontal alignment of the label when showing number and chart.
+     */
+    private int labelHorizontalAlignement = SwingConstants.RIGHT;
+    /**
+     * If true, a star is added after all the significant datasets. Only has an
+     * effect on SignificantStatisticalCategoryDataset.
+     */
+    private boolean indicateSignificance = false;
     /**
      * The background color used for the plots. For plots using light colors,
      * it's recommended to use a dark background color, and for plots using
@@ -222,13 +249,17 @@ public class JSparklinesErrorBarChartTableCellRenderer extends JPanel implements
         valueLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         valueLabel.setFont(valueLabel.getFont().deriveFont(valueLabel.getFont().getSize() - 2f));
 
+        signifianceLabel = new JLabel("");
+        signifianceLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+
         DefaultStatisticalCategoryDataset dataset = new DefaultStatisticalCategoryDataset();
         chart = ChartFactory.createBarChart(null, null, null, dataset, plotOrientation, false, false, false);
         this.chartPanel = new ChartPanel(chart);
 
         this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
         this.add(valueLabel);
-        add(chartPanel);
+        this.add(chartPanel);
+        this.add(signifianceLabel);
     }
 
     /**
@@ -368,11 +399,58 @@ public class JSparklinesErrorBarChartTableCellRenderer extends JPanel implements
             DefaultStatisticalCategoryDataset tempSet = (DefaultStatisticalCategoryDataset) value;
             this.setToolTipText("<html>Intensity: " + roundDouble((Double) tempSet.getMeanValue(0, 0), 4) + "<br>"
                     + "STDEV: " + roundDouble((Double) tempSet.getStdDevValue(0, 0), 4) + "</html>");
-        }
 
-        valueLabel.setMinimumSize(new Dimension(0, 0));
-        valueLabel.setSize(0, 0);
-        valueLabel.setVisible(false);
+            // check if significance is to be indicated
+            if (indicateSignificance && value instanceof SignificantStatisticalCategoryDataset) {
+                if (((SignificantStatisticalCategoryDataset) value).isSignificant() != null
+                        && ((SignificantStatisticalCategoryDataset) value).isSignificant()) {
+                    signifianceLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/asterisk.png")));
+                }
+            }
+
+            // make sure that the significance area is shown if needed
+            if (indicateSignificance) {
+                signifianceLabel.setMinimumSize(new Dimension(10, 0));
+            } else {
+                signifianceLabel.setMinimumSize(new Dimension(0, 0));
+            }
+
+            // show the number _and_ the chart if option selected
+            if (showNumberAndChart) {
+
+                // set the decimal format symbol
+                numberFormat.setDecimalFormatSymbols(DecimalFormatSymbols.getInstance(Locale.US));
+
+                double temp = tempSet.getMeanValue(0, 0).doubleValue();
+                valueLabel.setText(numberFormat.format(temp));
+
+                // We have to create a new color object because Nimbus returns
+                // a color of type DerivedColor, which behaves strange, not sure why.
+                Color bg = c.getBackground();
+                valueLabel.setBackground(new Color(bg.getRed(), bg.getGreen(), bg.getBlue()));
+
+                // add some padding
+                if (labelHorizontalAlignement == SwingConstants.RIGHT) {
+                    valueLabel.setText(valueLabel.getText() + "  ");
+                } else if (labelHorizontalAlignement == SwingConstants.LEFT) {
+                    valueLabel.setText("  " + valueLabel.getText());
+                }
+
+                valueLabel.setForeground(c.getForeground());
+
+                // set the horizontal text alignment and the label size
+                valueLabel.setHorizontalAlignment(labelHorizontalAlignement);
+                valueLabel.setMinimumSize(new Dimension(widthOfValueLabel, 0));
+                valueLabel.setSize(new Dimension(widthOfValueLabel, valueLabel.getPreferredSize().height));
+                valueLabel.setMaximumSize(new Dimension(widthOfValueLabel, valueLabel.getPreferredSize().height));
+                valueLabel.setPreferredSize(new Dimension(widthOfValueLabel, valueLabel.getPreferredSize().height));
+                valueLabel.setVisible(true);
+            } else {
+                valueLabel.setMinimumSize(new Dimension(0, 0));
+                valueLabel.setSize(0, 0);
+                valueLabel.setVisible(false);
+            }
+        }
 
         // respect focus and hightlighting
         setBorder(c.getBorder());
@@ -498,5 +576,87 @@ public class JSparklinesErrorBarChartTableCellRenderer extends JPanel implements
      */
     public void setTooltipLowerValue(double tooltipLowerValue) {
         this.tooltipLowerValue = tooltipLowerValue;
+    }
+
+    /**
+     * If true the number will be shown together with the bar chart in the cell.
+     * False only display the bar chart. This method is not to be confused with
+     * the showNumbers-method that only displays the numbers.
+     *
+     * @param showNumberAndChart if true the number and the chart is shown in
+     * the cell
+     * @param widthOfLabel the width used to display the label containing the
+     * number
+     */
+    public void showNumberAndChart(boolean showNumberAndChart, int widthOfLabel) {
+        showNumberAndChart(showNumberAndChart, widthOfLabel, numberFormat);
+    }
+
+    /**
+     * If true the number will be shown together with the bar chart in the cell.
+     * False only display the bar chart. This method is not to be confused with
+     * the showNumbers-method that only displays the numbers.
+     *
+     * @param showNumberAndChart if true the number and the chart is shown in
+     * the cell
+     * @param widthOfLabel the width used to display the label containing the
+     * number
+     * @param numberFormat the decimal format to use when showing the numbers
+     */
+    public void showNumberAndChart(boolean showNumberAndChart, int widthOfLabel, DecimalFormat numberFormat) {
+        this.showNumberAndChart = showNumberAndChart;
+        this.widthOfValueLabel = widthOfLabel;
+        this.numberFormat = numberFormat;
+    }
+
+    /**
+     * Set if the significance is to be indicated or not. Note that significance
+     * can only be indicated if SignificantStatisticalCategoryDataset is used.
+     *
+     * @param indicateSignificance
+     */
+    public void showSignificance(boolean indicateSignificance) {
+        this.indicateSignificance = indicateSignificance;
+    }
+
+    /**
+     * If true the number will be shown together with the bar chart in the cell.
+     * False only display the bar chart. This method is not to be confused with
+     * the showNumbers-method that only displays the numbers.
+     *
+     * @param showNumberAndChart if true the number and the chart is shown in
+     * the cell
+     * @param widthOfLabel the width used to display the label containing the
+     * number
+     * @param font the font to use for the label
+     * @param horizontalAlignement the horizontal alignment of the text in the
+     * label: one of the following constants defined in SwingConstants: LEFT,
+     * CENTER, RIGHT, LEADING or TRAILING.
+     */
+    public void showNumberAndChart(boolean showNumberAndChart, int widthOfLabel, Font font, int horizontalAlignement) {
+        showNumberAndChart(showNumberAndChart, widthOfLabel, font, horizontalAlignement, numberFormat);
+    }
+
+    /**
+     * If true the number will be shown together with the bar chart in the cell.
+     * False only display the bar chart. This method is not to be confused with
+     * the showNumbers-method that only displays the numbers.
+     *
+     * @param showNumberAndChart if true the number and the chart is shown in
+     * the cell
+     * @param widthOfLabel the width used to display the label containing the
+     * number
+     * @param font the font to use for the label
+     * @param horizontalAlignement the horizontal alignment of the text in the
+     * label: one of the following constants defined in SwingConstants: LEFT,
+     * CENTER, RIGHT, LEADING or TRAILING.
+     * @param numberFormat the decimal format to use when showing the numbers
+     */
+    public void showNumberAndChart(boolean showNumberAndChart, int widthOfLabel, Font font, int horizontalAlignement, DecimalFormat numberFormat) {
+        this.showNumberAndChart = showNumberAndChart;
+        this.widthOfValueLabel = widthOfLabel;
+        labelHorizontalAlignement = horizontalAlignement;
+        valueLabel.setFont(font);
+        this.numberFormat = numberFormat;
     }
 }
